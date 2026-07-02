46ca7c9c349c1f444861fecee9bccb148138731d
joshdan1000 <joshua.benoy.d@gmail.com>
2026-07-02 21:31:50 +0530

    Final changes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import matplotlib.pyplot as graph
 import numpy
+from rich.prompt import Prompt
 
 # CONSTANTS 
 gravity = 9.80665
-initialHeight = 100
-initialVelocity = 10
+
+print("\n\n")
+initialHeight = float(Prompt.ask("[bold light_green]Initial height of the projectile (meters): [/bold light_green]"))
+if initialHeight < 0:
+    initialHeight = 0
+
+initialVelocity = float(Prompt.ask("[bold light_green]Initial velocity of the projectile (m/s): [/bold light_green]"))
+if initialVelocity < 0:
+    initialVelocity = 0
+print("\n\n")
+
 
 def xComponent(angle):
     return initialVelocity * numpy.cos(numpy.radians(angle))
